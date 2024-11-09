555a16f0d2f0ec5f09521bd71f95147f65e9d9b5
김경모 <gm.kim333@gmail.com>
2024-11-09 18:28:12 +0900

    style 틴트컬러 변경

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,24 +3,24 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = "#0a7ea4";
-const tintColorDark = "#fff";
+const tintColorLight = "#FF6B6B";
+const tintColorDark = "#4ECDC4";
 
 export const Colors = {
     light: {
-        text: "#11181C",
-        background: "#fff",
+        text: "#2D3436",
+        background: "#FFFFFF",
         tint: tintColorLight,
-        icon: "#687076",
-        tabIconDefault: "#687076",
+        icon: "#636E72",
+        tabIconDefault: "#B2BEC3",
         tabIconSelected: tintColorLight,
     },
     dark: {
-        text: "#ECEDEE",
-        background: "#151718",
+        text: "#DFE6E9",
+        background: "#2D3436",
         tint: tintColorDark,
-        icon: "#9BA1A6",
-        tabIconDefault: "#9BA1A6",
+        icon: "#81ECEC",
+        tabIconDefault: "#74B9FF",
         tabIconSelected: tintColorDark,
     },
 };
